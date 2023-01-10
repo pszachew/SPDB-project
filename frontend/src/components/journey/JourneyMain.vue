89dd9b0edc7e4f6b67f55c91dda4f75e542c641d
kacper kostecki <k.kostecki99@gmail.com>
2023-01-10 21:39:20 +0100

    adding markers when new place chosen

--- frontend/src/components/journey/JourneyMain.vue
+++ frontend/src/components/journey/JourneyMain.vue
@@ -18,7 +18,7 @@
 					<div class="col-md-12 min-vh-80 d-flex flex-column border rounded">
 						<div class="row">
 							<div class="col-12">
-								<table class="table table-hover" v-if="chosenPlaces.length > 0">
+								<table class="table" v-if="chosenPlaces.length > 0">
 								<thead>
 									<tr>
 										<th scope="col">Address</th>
@@ -28,9 +28,8 @@
 									</tr>
 								</thead>
 								<tbody>
-									<tr v-for="place of chosenPlaces" :key="place">
-										<JourneyRecord @placeAccepted="onPlaceAccepted" @placeRemoved="onPlaceRemoved" :place="place" />
-									</tr>
+									<JourneyRecord v-for="place of chosenPlaces" :key="place" :place="place"
+													@placeAccepted="onPlaceAccepted" @placeRemoved="onPlaceRemoved"/>
 								</tbody>
 								</table>
 								<div v-else>
@@ -39,7 +38,7 @@
 							</div>
 						</div>
 						<div class="row mt-auto">
-							<button class="btn btn-primary" @click="this.$store.commit('setIsLoading', true)">Generate journey</button>
+							<button class="btn btn-primary" @click="calculateJourney">Generate journey</button>
 						</div>
 					</div>
 				</div>
@@ -67,28 +66,52 @@ export default {
 		JourneyRecord,
 	},
     mounted(){
-		let map = L.map('map', {
-			scrollWheelZoom: true
-		});
-		this.map = map
-		map.setView([52.232, 21.028], 12);
+		this.map = L.map('map', {scrollWheelZoom: true});
+		this.map.setView([52.232, 21.028], 12);
 		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			maxZoom: 19,
-		}).addTo(map);
+		}).addTo(this.map);
 		const search = new GeoSearchControl({
 			provider: new OpenStreetMapProvider(),
 			style: 'bar',
+			showMarker: false,
+			autoClose: true,
+			keepResult: false,
 		});
-
-		map.addControl(search);
-		map.on('geosearch/showlocation', (event) => {
+		this.map.addControl(search);
+		this.map.on('geosearch/showlocation', (event) => {
+			for (const place of this.chosenPlaces) {
+				if (!place.accepted) 
+					this.map.removeLayer(place.marker);
+			}
+			this.chosenPlaces = this.chosenPlaces.filter(function( obj ) {
+				return obj.accepted;
+			});
+			let randomColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+			let marker = new L.Marker([event.location.y, event.location.x], {
+				icon: this.setupIcon(randomColor)
+			});
+			marker.bindPopup(event.location.label);
+			let identifier = this.placesCounter;
 			this.chosenPlaces.push({
-				identifier: this.placesCounter,
-				address: event.marker.getPopup().getContent(),
+				identifier: identifier,
+				address: event.location.label,
 				lat: event.location.y,
 				lng: event.location.x,
+				marker: marker,
+				accepted: false,
+				color: randomColor,
 			});
-			console.log(this.chosenPlaces)
+			const index = this.chosenPlaces.map(e => e.identifier).indexOf(identifier);
+			this.chosenPlaces.at(index).marker.addTo(this.map);
+			marker.on("mouseover", () => {
+				this.chosenPlaces.at(index).highlighted = true
+				marker.openPopup();
+			})
+			marker.on("mouseout", () => {
+				this.chosenPlaces.at(index).highlighted = false
+				marker.closePopup();
+			})
 			this.placesCounter++;
 		});
     },
@@ -99,19 +122,45 @@ export default {
 		}
 	},
 	methods: {
+		setupIcon(color) {
+			const markerHtmlStyles = `
+				background-color: ${color};
+				width: 2rem;
+				height: 2rem;
+				display: block;
+				left: -1rem;
+				top: -1rem;
+				position: relative;
+				border-radius: 3rem 3rem 0;
+				transform: rotate(45deg);
+				border: 1px solid #FFFFFF`
+			return L.divIcon({
+				className: "custom-pin",
+				iconAnchor: [0, 24],
+				labelAnchor: [-6, 0],
+				popupAnchor: [0, -36],
+				html: `<span style="${markerHtmlStyles}" />`
+			})
+		},
 		onPlaceAccepted(placeIdentifier) {
-			console.log("accepted", placeIdentifier)
+			const index = this.chosenPlaces.map(e => e.identifier).indexOf(placeIdentifier);
+			this.chosenPlaces[index].accepted = true;
 		},
 		onPlaceRemoved(placeIdentifier) {
+			const index = this.chosenPlaces.map(e => e.identifier).indexOf(placeIdentifier);
+			this.map.removeLayer(this.chosenPlaces[index].marker);
 			this.chosenPlaces = this.chosenPlaces.filter(function( obj ) {
 				return obj.identifier !== placeIdentifier;
 			});
+		},
+		calculateJourney() {
+			this.$store.commit('setIsLoading', true)
 		}
 	}
 }
 </script>
 
-<style>
+<style scoped>
 #map{
 	width: 100%;
 	height: 100%;
--- frontend/src/components/journey/JourneyRecord.vue
+++ frontend/src/components/journey/JourneyRecord.vue
@@ -1,22 +1,24 @@
 <template>
-    <td>{{ place.address }}</td>
-    <td><Datepicker v-model="openningHours" range time-picker /></td>
-    <td><Datepicker v-model="visitingTime" time-picker /></td>
-    <td>
-        <div class="no-break" v-if="!accepted">
-            <button class="btn btn-outline-success mx-1" @click="acceptPlace()">
-                <font-awesome-icon icon="fa-solid fa-check"/>
-            </button>
-            <button class="btn btn-outline-danger mx-1" @click="removePlace()">
-                <font-awesome-icon icon="fa-solid fa-x"/>
-            </button>
-        </div>
-        <div v-else>
-            <button class="btn btn-danger mx-1" @click="removePlace()">
-                <font-awesome-icon icon="fa-solid fa-trash-can"/>
-            </button>
-        </div>
-    </td>
+    <tr ref="row" class="row-border" :class="{highlighted: place.highlighted}">
+        <td>{{ place.address }}</td>
+        <td><Datepicker v-model="openningHours" range time-picker /></td>
+        <td><Datepicker v-model="visitingTime" time-picker /></td>
+        <td>
+            <div class="no-break" v-if="!accepted">
+                <button class="btn btn-outline-success mx-1" @click="acceptPlace()">
+                    <font-awesome-icon icon="fa-solid fa-check"/>
+                </button>
+                <button class="btn btn-outline-danger mx-1" @click="removePlace()">
+                    <font-awesome-icon icon="fa-solid fa-x"/>
+                </button>
+            </div>
+            <div v-else>
+                <button class="btn btn-danger mx-1" @click="removePlace()">
+                    <font-awesome-icon icon="fa-solid fa-trash-can"/>
+                </button>
+            </div>
+        </td>
+    </tr>
 </template>
 <script>
 import { ref } from 'vue';
@@ -35,7 +37,7 @@ export default {
         Datepicker
     },
     props: [
-        "place"
+        "place",
     ],
     emits: ["placeAccepted", "placeRemoved"],
     setup() {
@@ -68,8 +70,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .no-break{
     white-space: nowrap
+}
+.row-border{
+    border-bottom: 2px solid v-bind('place.color')
+}
+.highlighted {
+    background-color: v-bind('place.color')
 }
 </style>
